129a64f6408600da60d2a959ad8087e6d572160b
lixin <1121705040@qq.com>
2021-05-09 15:06:21 +0800

    第一周作业

---
+++ src/week1/HelloClassLoader.java
@@ -0,0 +1,44 @@
+package week1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * @Author: LiXin
+ * @CreateTime: 2021/05/06/ 12:55
+ * @Presentation:
+ */
+public class HelloClassLoader extends ClassLoader{
+    public static void main(String[] args) throws Exception {
+        //得到byte数组
+        byte[] bytes = getBytes();
+        //遍历byte数组,转换字节
+        byteFor(bytes);
+        //调用hello方法
+        invokeMethod(bytes);
+    }
+
+    private static void invokeMethod(byte[] bytes) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        HelloClassLoader he = new HelloClassLoader();
+        Class<?> aClass = he.defineClass("Hello", bytes, 0, bytes.length);
+        Object o = aClass.newInstance();
+        Method hello = aClass.getMethod("hello");
+        hello.invoke(o);
+    }
+
+    private static void byteFor(byte[] bytes) {
+        for (int i=0;i<bytes.length;i++) {
+            bytes[i] = (byte) (255 - bytes[i]);
+        }
+    }
+
+    private static byte[] getBytes() throws IOException {
+        FileInputStream in = new FileInputStream(HelloClassLoader.class.getResource("Hello.xlass").getPath());
+        byte[] bytesTmp = new byte[1024*4];
+        int length = in.read(bytesTmp);
+        return Arrays.copyOf(bytesTmp,length);
+    }
+}
