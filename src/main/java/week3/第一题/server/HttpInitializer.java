2f9ba6f05a7d04e6ab036863afab91a8aea702e9
lixin <1121705040@qq.com>
2021-05-23 16:49:17 +0800

    第一题

---
+++ src/main/java/week3/第一题/server/HttpInitializer.java
@@ -0,0 +1,20 @@
+package week3.第一题.server;
+
+
+import org.apache.flink.shaded.netty4.io.netty.channel.ChannelInitializer;
+import org.apache.flink.shaded.netty4.io.netty.channel.ChannelPipeline;
+import org.apache.flink.shaded.netty4.io.netty.channel.socket.SocketChannel;
+import org.apache.flink.shaded.netty4.io.netty.handler.codec.http.HttpObjectAggregator;
+import org.apache.flink.shaded.netty4.io.netty.handler.codec.http.HttpServerCodec;
+
+public class HttpInitializer extends ChannelInitializer<SocketChannel> {
+
+	@Override
+	public void initChannel(SocketChannel ch) {
+		ChannelPipeline p = ch.pipeline();
+		p.addLast(new HttpServerCodec());
+		//p.addLast(new HttpServerExpectContinueHandler());
+		p.addLast(new HttpObjectAggregator(1024 * 1024));
+		p.addLast(new HttpHandler());
+	}
+}
